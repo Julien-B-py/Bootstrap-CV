167537f97e6d57a8d18c8bbc0d59ecf7dfc3e9a7
Julien-B-py <86344743+Julien-B-py@users.noreply.github.com>
2022-01-07 11:23:14 +0100

    v1.6.3
    Add animations.
    Edit CSS to make it mobile first.
    Edit footer.
    Fix navbar display for mobile.

--- js/script.js
+++ js/script.js
@@ -1,21 +1,89 @@
-let controller =  new ScrollMagic.Controller();
-let timeline = new TimelineMax();
+ScrollTrigger.batch(".resume p", {
+  onEnter: elements => {
+    gsap.from(elements, {
+      scrollTrigger: {
+        trigger: elements,
+        start: "center bottom",
+
+        toggleActions: "play none none none",
+
+      },
+
+      autoAlpha: 0,
+      scale: 0,
+      stagger: 0.3
+    });
+  },
+  once: true
+});
 
 
-timeline
-.fromTo(".icon-container", {opacity:0},{opacity:1, duration:1})
-.fromTo(".contact-text", {opacity:0},{opacity:1, duration:1})
-;
 
-let scene= new ScrollMagic.Scene({
-    triggerElement: "#contact",
-    duration: "70%",
-    triggerHook:  'onEnter',
-})
-.setTween(timeline)
-.addTo(controller);
+ScrollTrigger.batch(".skill-card", {
+  onEnter: elements => {
+    gsap.from(elements, {
+      scrollTrigger: {
+        trigger: elements,
+        start: "center bottom",
+
+        toggleActions: "play none none none",
+
+      },
+      bottom: "-100%",
+      autoAlpha: 0,
+      scale: 0,
+      stagger: 0.1
+    });
+  },
+  once: true
+});
+
+ScrollTrigger.batch(".edu-card", {
+  onEnter: elements => {
+    gsap.from(elements, {
+      scrollTrigger: {
+        trigger: elements,
+        start: "center bottom",
+
+        toggleActions: "play none none none",
+
+      },
+      right: "-200%",
+      autoAlpha: 0,
+      scale: 0,
+      stagger: 0.7
+    });
+  },
+  once: true
+});
 
 
+gsap.from(".divers-container", {
+  scrollTrigger: {
+    trigger: ".divers-container",
+    start: "center bottom",
 
+    toggleActions: "play none none none",
 
+  },
+  y:'-50%',
+  autoAlpha: 0,
+  scale: 0,
+  stagger: 0.1,
+});
 
+var tl = gsap.timeline({scrollTrigger: {
+    trigger: "#footer",
+        start: "center bottom",
+    toggleActions: "play none none none",
+  }})
+tl.from(".footer-icon", {scale:0})
+tl.from("#footer p", {scale:0})
+
+var tl2 = gsap.timeline({scrollTrigger: {
+    trigger: "#contact",
+        start: "center bottom",
+    toggleActions: "play none none none",
+  }})
+tl2.from(".icon-container", {scale:0})
+tl2.from(".contact-text", {x:'+200%', autoAlpha:0, scale:0})
